ffed685cbd7ee71fb960882f12d82cda36ae12fb
Gabriel416 <gferguson954@gmail.com>
2018-02-24 14:01:56 -0500

    latest twilio

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Classroom;
+use App\Professional;
+// Use the REST API Client to make requests to the Twilio REST API
+use Twilio\Rest\Client;
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\VideoGrant;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Your Account SID and Auth Token from twilio.com/console
+        $sid = env("TWILIO_ACCOUNT")
+        $token = env("TWILIO_SECRET");
+        $client = new Client($sid, $token);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/assets/js/VideoChat/Video.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+    <section>
+        <div style="color: black; font-size: 54px;">VIDEO CHAT WOULD BE HERE</div>
+    </section>
+</template>
+
+<script>
+//GETTERS
+// import { mapGetters } from 'vuex';
+export default {
+  data: function() {
+    return {
+      loaded: false
+    };
+  },
+
+  mounted() {
+    console.log("Component mounted.");
+    Twilio.Video.connect(
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2Y1MzE5MTExNmZiNGQwOGRmMmMyYzkzNTMwZjE2YmUyLTE1MTk0OTc2NzAiLCJpc3MiOiJTS2Y1MzE5MTExNmZiNGQwOGRmMmMyYzkzNTMwZjE2YmUyIiwic3ViIjoiQUM3NTM1MWUxMDdiNDMwNGIwMDdhY2I3NGQwYzZlOWFmMCIsImV4cCI6MTUxOTUwMTI3MCwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiR2FicmllbCIsInZpZGVvIjp7InJvb20iOiJ0ZXN0In19fQ.vboM6A9_-qIipBoGLC_fEaeEr_EyvaO-xhMHvsoXli8",
+      { name: "my-new-room" }
+    ).then(
+      function(room) {
+        console.log("Successfully joined a Room: ", room);
+        room.on("participantConnected", function(participant) {
+          console.log("A remote Participant connected: ", participant);
+        });
+      },
+      function(error) {
+        console.error("Unable to connect to Room: " + error.message);
+      }
+    );
+  },
+
+  methods: {}
+};
+</script>
+<style>
+
+</style>
